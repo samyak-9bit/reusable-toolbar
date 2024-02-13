63fdc850580bc17c97a39cecf07787be790f41ec
Samyak <samyak.patni@ninebit.in>
2024-02-13 17:55:18 +0530

    13/2/24

--- index.js
+++ index.js
@@ -1,61 +1,111 @@
-class NineToolbar extends HTMLElement {
-    // private shadow: ShadowRoot;
+const navbar = document.createElement('template');
+navbar.innerHTML = `
+<style>
+  header {
+    display: flex;
+    align-items: center;
+    min-height: 50px;
+    max-height: 100px;
+    justify-content: space-between;
+    width: 100%;
+    z-index: 99;
+    background-color: rgb(35, 180, 232);
+  }
+  .logo {
+    color: #333333;
+    font-family: 'Arial Black', Gadget, sans-serif;
+    font-size: 40px;
+    text-align: center;
+    margin-left: 5px;
+  }
+  nav {
+    margin: auto;
+  }
+  form {
+    display: inline-flex;
+    border: 1px solid #616364;
+    margin: 20px;
+    padding: 5px;
+    background-color:white;
+  }
+  input {
+    height: 25px;
+    border: none;
+    padding: 10px;
+    flex-grow: 1;
+    outline: none;
+  }
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    padding: 10px;
+  }
+</style>
 
-    constructor() {
-        super();
-        this.title = this.getAttribute('toolbar-title');
-        this.showSearch = this.getAttribute('showSearch')||true;
-        if(this.showSearch===true){
-        this.searchPlaceholder = this.getAttribute('searchPlaceholder') || 'Default Placeholder';
-        // this.handleSearch = this.getAttribute('handleSearch');
-        this.shadow = this.attachShadow({mode: 'open'});
+<header>
+  <div>
+    <slot name="brand" class="logo"></slot>
+  </div>
+  <nav>
+    <slot></slot>
+  </nav>
+</header>
+`;
+
+class Navbar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(navbar.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    const userInput = {
+      title: this.getAttribute('title'),
+      showSearchBar: this.getAttribute('showSearchBar'),
+      searchPlaceholder: this.getAttribute('searchPlaceholder'),
+      handleSearch: this.getAttribute('handle-search'),
+    };
+
+    const brandSlot = this.shadowRoot.querySelector('.logo');
+    const titleTextNode = document.createTextNode(userInput.title);
+    brandSlot.appendChild(titleTextNode);
+
+    if (userInput.showSearchBar === 'true') {
+      const navSlot = this.shadowRoot.querySelector('nav');
+      const form = document.createElement('form');
+      const input = document.createElement('input');
+      input.setAttribute('type', 'text');
+      input.setAttribute('placeholder', userInput.searchPlaceholder || 'Default Placeholder');
+      const submitButton = document.createElement('button');
+      submitButton.setAttribute('type', 'submit');
+      submitButton.textContent = 'GO';
+      form.appendChild(input);
+      form.appendChild(submitButton);
+      navSlot.appendChild(form);
+
+      form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const inputValue = input.value;
+        const searchEvent = new CustomEvent('search', { detail: inputValue });
+        document.dispatchEvent(searchEvent); // Dispatch the custom event
+        if (userInput.handleSearch && window[userInput.handleSearch] && typeof window[userInput.handleSearch] === 'function') {
+            window[userInput.handleSearch](inputValue); // Call the provided function passing input value
+        } else {
+            console.error('Invalid handleSearch function provided');
         }
-    }
+    });
+    
 
-    connectedCallback() {
-        this.render();
+      // submitButton.addEventListener('click', () => {
+      //   if (userInput.handleSearch) {
+      //     this.dispatchEvent(new CustomEvent('handleSearch', { detail: input.value }));
+      //   }
+      // });
     }
-
-    render() {
-        this.shadow.innerHTML = `
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li><hr class="dropdown-divider"></li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-              </li>
-            </ul>
-            <form class="d-flex" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
-        `;
-    }
+}
 
 }
-customElements.define('nine-toolbar', NineToolbar);
+
+window.customElements.define('nine-toolbar', Navbar);
